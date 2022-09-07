7a319df7e32222faad3c0107a04c9894832fb0de
jagardner21 <42302459+jagardner21@users.noreply.github.com>
2022-09-07 16:17:12 -0700

    added cart with hard coded pizzas

---
+++ src/components/Cart.tsx
@@ -0,0 +1,43 @@
+import React from "react";
+import { FiShoppingCart } from "react-icons/fi"
+import CartCSS from "./Cart.module.css"
+
+interface Props {}
+
+interface State {
+    isOpen: boolean
+}
+
+class Cart extends React.Component<Props, State> {
+    constructor(props: Props) {
+        super(props)
+        this.state = {
+            isOpen: false
+        }
+    }
+
+    render() {
+        return (
+            <div className={CartCSS.cartContainer}>
+                <button type="button" className={CartCSS.button} onClick={() => {
+                    this.setState((prevState) => ({isOpen: !prevState.isOpen}))
+                }}>
+                    <FiShoppingCart />
+                    <span>2 pizza(s)</span></button>
+                <div 
+                    className={CartCSS.cartDropDown}
+                    style={{
+                        display: this.state.isOpen ? "block" : "none"
+                    }}
+                >
+                    <ul>
+                        <li>Pepperoni</li>
+                        <li>Black Olive</li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Cart
